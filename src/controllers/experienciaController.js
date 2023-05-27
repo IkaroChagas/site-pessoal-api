b09c11477cd4e3ad46e880fdeffa1c2c56029006
Ikaro <ikarochagas.ig@gmail.com>
2023-05-27 10:04:50 -0300

    controller updated

--- src/controllers/experienciaController.js
+++ src/controllers/experienciaController.js
@@ -1,30 +1,31 @@
 const experienciasRepository = require('../repositories/experienciasRepository');
 
-exports.getAllExperiencias = async (req, res) => {
-    const experiencias = await experienciasRepository.getAllExperiencias();
+exports.getAllExperiencias = async (req, res, next) => {
+    const tipo = req.query.tipo;
+    const experiencias = await experienciasRepository.getAllExperiencias(tipo);
     res.json(experiencias);
 }
 
-exports.getExperienciasById = async (req, res) => {
+exports.getExperienciasById = async (req, res, next) => {
     const id = parseInt(req.params.id);
     const experiencias = await experienciasRepository.getExperienciasById(id);
     res.json(experiencias);
 };
 
-exports.createExperiencia = async (req, res) => {
+exports.createExperiencia = async (req, res, next) => {
     const experiencia = req.body;
     const newExperiencia = await experienciasRepository.createExperiencia(experiencia);
     res.json(newExperiencia);
 };
 
-exports.updateExperiencia = async (req, res) => {
+exports.updateExperiencia = async (req, res, next) => {
     const id = parseInt(req.params.id);
     const experiencia = req.body;
     const updateExperiencia = await experienciasRepository.updateExperiencia(id, experiencia);
     res.json(updateExperiencia);
 };
 
-exports.deleteExperiencia = async (req, res) => {
+exports.deleteExperiencia = async (req, res, next) => {
     const id = parseInt(req.params.id);
     await experienciasRepository.deleteExperiencia(id);
     res.json({message: `Experiencia ${id} deletada`});

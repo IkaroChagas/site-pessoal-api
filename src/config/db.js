df313e9ed308cf7238a3909ffcc13ed65d8fd65f
Ikaro <ikarochagas.ig@gmail.com>
2023-05-30 11:28:31 -0300

    experienciasRepository updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg');
 
-
+const dbValues = {
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+}
 // variáveis de config do servidor
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
+const pool = new Pool(dbValues);
 
 
 
--- src/repositories/experienciasRepository.js
+++ src/repositories/experienciasRepository.js
@@ -1,7 +1,7 @@
 const { pool } = require('../config/db');
 
 exports.getAllExperiencias = async (tipo) => {
-        let query = "SELECT * FROM experiencia";
+        let query = "SELECT * FROM experiencias";
         if (tipo) {
                 query += `WHERE tipo = '${tipo}'`;      
         }
@@ -10,15 +10,15 @@ exports.getAllExperiencias = async (tipo) => {
 };
 
 exports.getExperienciasById =  async (id) => {
-        const result = await pool.query("SELECT * FROM experiencia WHERE id = $1", {id});
-        return result.rows;
+        const result = await pool.query("SELECT * FROM experiencias WHERE id = $1", [id]);
+        return result.rows[0];
 };
 
 exports.createExperiencia = async (experiencia) => { 
         const result = await pool.query(`
-        INSERT INTO experiencia (titulo, tipo, descricao, anoInicio, anoFim)
-        VALUES ($1, $2, $3, $4);
-        RETURNING *
+        INSERT INTO experiencias (titulo, tipo, descricao, anoInicio, anoFim)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *;
         `,
        [experiencia.titulo, experiencia.tipo, experiencia.descricao, experiencia.anoInicio, experiencia.anoFim])
         return result.rows[0]
@@ -26,15 +26,15 @@ exports.createExperiencia = async (experiencia) => {
 
 exports.updateExperiencia = async (id, experiencia) => {
         const result = await pool.query(`
-                UPDATE experiencia
+                UPDATE experiencias
                 SET titulo = $1, tipo = $2, descricao = $3 , anoInicio = $4, anoFim = $5
-                WHERE id = ?
-                RETURNING *
+                WHERE id = $6
+                RETURNING *;
         `,
-        [experiencia.titulo, experiencia.tipo, experiencia.descricao, experiencia.anoInicio, experiencia.anoFim])
+        [experiencia.titulo, experiencia.tipo, experiencia.descricao, experiencia.anoInicio, experiencia.anoFim, id])
         return result.rows[0]  
 }
 
 exports.deleteExperiencia = async (id) => {
-        await pool.query("DELETE FROM experiencia WHERE id = $1", [id])
+        await pool.query("DELETE FROM experiencias WHERE id = $1", [id])
 }
